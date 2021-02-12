0b3562b29b50d7a08481b2155227ccd9cba40120
Luiz Chequini <luizchequini@gmail.com>
2021-02-12 06:57:50 -0300

    Criando um APP simples de cadastro de séries em .NET - Parte 1 e Parte 2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dio_bootcamp_localiza_mrv_app_console_dotnet_cadastro_series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
